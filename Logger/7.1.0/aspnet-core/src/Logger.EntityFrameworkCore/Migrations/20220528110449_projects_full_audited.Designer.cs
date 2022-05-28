058e496d80eaac11e8b38e5349fb1d89c54eca92
Sandi <33003402+sandi7s@users.noreply.github.com>
2022-05-28 13:08:56 +0200

    projects fullAudited

--- Logger/7.1.0/aspnet-core/src/Logger.EntityFrameworkCore/Migrations/20220528110449_projects_full_audited.Designer.cs
+++ Logger/7.1.0/aspnet-core/src/Logger.EntityFrameworkCore/Migrations/20220528110449_projects_full_audited.Designer.cs
@@ -4,6 +4,7 @@ using Logger.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Logger.Migrations
 {
     [DbContext(typeof(LoggerDbContext))]
-    partial class LoggerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220528110449_projects_full_audited")]
+    partial class projects_full_audited
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Logger/7.1.0/aspnet-core/src/Logger.EntityFrameworkCore/Migrations/20220528110449_projects_full_audited.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Logger.Migrations
+{
+    public partial class projects_full_audited : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "Project",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatorUserId",
+                table: "Project",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "DeleterUserId",
+                table: "Project",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "Project",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Project",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModificationTime",
+                table: "Project",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "LastModifierUserId",
+                table: "Project",
+                type: "bigint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorUserId",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "DeleterUserId",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "LastModificationTime",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierUserId",
+                table: "Project");
+        }
+    }
+}
