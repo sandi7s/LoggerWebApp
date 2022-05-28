d034591ee026bfbe9e2c2493ff53ad300c6e400b
Sandi <33003402+sandi7s@users.noreply.github.com>
2022-05-28 13:17:43 +0200

    log entryes audited

--- Logger/7.1.0/aspnet-core/src/Logger.Core/LogEntry/LogEntry.cs
+++ Logger/7.1.0/aspnet-core/src/Logger.Core/LogEntry/LogEntry.cs
@@ -1,4 +1,5 @@
 ﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Logger.LogEntry
 {
-    public class LogEntry : Entity
+    public class LogEntry : FullAuditedEntity
     {
         public string Log { get; set; }
         public string Severity { get; set; }
---
+++ Logger/7.1.0/aspnet-core/src/Logger.EntityFrameworkCore/Migrations/20220528111012_logs_full_audited.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Logger.Migrations
+{
+    public partial class logs_full_audited : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "LogEntry",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatorUserId",
+                table: "LogEntry",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "DeleterUserId",
+                table: "LogEntry",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "LogEntry",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "LogEntry",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModificationTime",
+                table: "LogEntry",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "LastModifierUserId",
+                table: "LogEntry",
+                type: "bigint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "LogEntry");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorUserId",
+                table: "LogEntry");
+
+            migrationBuilder.DropColumn(
+                name: "DeleterUserId",
+                table: "LogEntry");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "LogEntry");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "LogEntry");
+
+            migrationBuilder.DropColumn(
+                name: "LastModificationTime",
+                table: "LogEntry");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierUserId",
+                table: "LogEntry");
+        }
+    }
+}
