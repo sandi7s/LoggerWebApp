add8c36c21e0ec35c9907e4fc27170dcaf68420c
Sandi <33003402+sandi7s@users.noreply.github.com>
2022-05-29 22:48:31 +0200

    tests

---
+++ Logger/7.1.0/aspnet-core/test/Logger.Tests/LogEntryes/LogEntryes_Tests.cs
@@ -0,0 +1,94 @@
+﻿using Logger.LogEntry;
+using Logger.LogEntry.Dto;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Logger.Tests.LogEntryes
+{
+    public class LogEntryes_Tests : LoggerTestBase
+    {
+        private readonly ILogEntryAppService _logEntryAppService;
+
+        public LogEntryes_Tests()
+        {
+            _logEntryAppService = Resolve<ILogEntryAppService>();
+        }
+
+        [Fact]
+        public async Task GetLogEntrys_Test()
+        {
+            await _logEntryAppService.CreateAsync(
+                new LogEntry.Dto.CreateLogEntryDto
+                {
+                    Log = "LogEntry 1yxcvbnm",
+                    Severity = "www.logEntry1.com",
+                    TimeStamp = DateTime.UtcNow,
+                    ProjectId = 0
+                });
+
+            var output = await _logEntryAppService.GetAllAsync(new PagedLogEntryResultRequestDto { MaxResultCount = 20, SkipCount = 0 });
+
+            output.Items.Count.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task CRUDLogEntry_Test()
+        {
+            // Act
+            await _logEntryAppService.CreateAsync(
+                new LogEntry.Dto.CreateLogEntryDto
+                {
+                    Log = "LogEntry 1yxcvbnm",
+                    Severity = "www.logEntry1.com",
+                    TimeStamp = DateTime.UtcNow,
+                    ProjectId = 0
+                });
+
+            int id = 1;
+
+            await UsingDbContextAsync(async context =>
+            {
+                var newLogEntry = context.LogEntry.Where(e => e.Log == "LogEntry 1yxcvbnm").FirstOrDefault();
+                newLogEntry.ShouldNotBeNull();
+                id = newLogEntry.Id;
+            });
+
+            var logEntry = await _logEntryAppService.GetAsync(
+                new LogEntry.Dto.LogEntryDto
+                {
+                    Id = id,
+                });
+
+            logEntry.ShouldNotBeNull();
+
+            logEntry.Log = "updated 1yxcvbnm";
+
+            await _logEntryAppService.UpdateAsync(logEntry);
+
+            await UsingDbContextAsync(async context =>
+            {
+                var updatedLogEntry = context.LogEntry.Where(e => e.Log == "updated 1yxcvbnm").FirstOrDefault();
+                updatedLogEntry.ShouldNotBeNull();
+                id = updatedLogEntry.Id;
+
+                await _logEntryAppService.DeleteAsync(
+                new LogEntry.Dto.LogEntryDto
+                {
+                    Id = id,
+                });
+            });
+
+            await UsingDbContextAsync(async context =>
+            {
+                var deletedLogEntry = context.LogEntry.Where(e => e.Id == id).FirstOrDefault();
+
+                deletedLogEntry.IsDeleted.ShouldBe(true);
+            });
+        }
+    }
+}
---
+++ Logger/7.1.0/aspnet-core/test/Logger.Tests/Projects/Projects_Tests.cs
@@ -0,0 +1,92 @@
+﻿using Logger.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using Logger.Project.Dto;
+
+namespace Logger.Tests.Projects
+{
+
+    public class Projects_Tests : LoggerTestBase
+    {
+        private readonly IProjectAppService _projectAppService;
+
+        public Projects_Tests()
+        {
+            _projectAppService = Resolve<IProjectAppService>();
+        }
+
+        [Fact]
+        public async Task GetProjects_Test()
+        {
+            await _projectAppService.CreateAsync(
+                new Project.Dto.CreateProjectDto
+                {
+                    Name = "Project 1yxcvbnm",
+                    Url = "www.project1.com",
+                    ShowOnFrontPage = true
+                });
+
+            var output = await _projectAppService.GetAllAsync(new PagedProjectResultRequestDto { MaxResultCount = 20, SkipCount = 0 });
+
+            output.Items.Count.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task CRUDProject_Test()
+        {
+            // Act
+            await _projectAppService.CreateAsync(
+                new Project.Dto.CreateProjectDto
+                {
+                    Name = "Project 1yxcvbnm",
+                    Url = "www.project1.com",
+                    ShowOnFrontPage = true
+                });
+
+            int id = 1;
+            await UsingDbContextAsync(async context =>
+            {
+                var newProject = context.Project.Where(e => e.Name == "Project 1yxcvbnm").FirstOrDefault();
+                newProject.ShouldNotBeNull();
+                id = newProject.Id;
+            });
+
+            var project = await _projectAppService.GetAsync(
+                new Project.Dto.ProjectDto
+                {
+                    Id = id,
+                });
+
+            project.ShouldNotBeNull();
+
+            project.Name = "updated name 1yxcvbnm";
+
+            await _projectAppService.UpdateAsync(project);
+
+            await UsingDbContextAsync(async context =>
+            {
+                var updatedProject = context.Project.Where(e => e.Name == "updated name 1yxcvbnm").FirstOrDefault();
+                updatedProject.ShouldNotBeNull();
+                id = updatedProject.Id;
+
+                await _projectAppService.DeleteAsync(
+                new Project.Dto.ProjectDto
+                {
+                    Id = id,
+                });
+            });
+
+            await UsingDbContextAsync(async context =>
+            {
+                var deletedProject = context.Project.Where(e => e.Id == id).FirstOrDefault();
+
+                deletedProject.IsDeleted.ShouldBe(true);
+            });
+        }
+    }
+}
